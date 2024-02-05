3b214f4581fea876bc4fc908c4c1bce31fd43c48
Kaadirm <141996672+Kaadirm@users.noreply.github.com>
2024-02-05 16:57:00 +0300

    Sync Vs Async differences

--- 01-node-tutorial/10-fs-sync.js
+++ 01-node-tutorial/10-fs-sync.js
@@ -1,4 +1,5 @@
 // const fs = require("fs");
+console.log("start");
 const { readFileSync, writeFileSync, write } = require("fs");
 
 const first = readFileSync("./content/first.txt", "utf8");
@@ -9,3 +10,7 @@ const second = readFileSync("./content/second.txt", "utf8");
 writeFileSync("./content/result-sync.txt", `Here is the result ----- ${first} ----- ${second}`, {flag: "a"})
 
 console.log(readFileSync("./content/result-sync.txt", "utf8"));
+
+
+console.log("done with this task");
+console.log("starting the next one");
--- 01-node-tutorial/11-fs-async.js
+++ 01-node-tutorial/11-fs-async.js
@@ -7,6 +7,7 @@ const { readFile, writeFile, write } = require("fs");
 //         console.log(result)
 //     }
 // } )
+console.log("start");
 readFile("./content/first.txt", "utf8", (err, result) => {
     if(err){
         console.log(err);
@@ -26,7 +27,9 @@ readFile("./content/first.txt", "utf8", (err, result) => {
                 console.log(err);
                 return
             }
-            console.log(result);
+            console.log("done with this task");
         })
     })
 } )
+
+console.log("starting next task");
