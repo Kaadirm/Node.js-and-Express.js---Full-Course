b35aceb95e903d29e0e55eddee71fb3531f25f6f
Kaadirm <141996672+Kaadirm@users.noreply.github.com>
2024-02-05 13:40:46 +0300

    Built-In Module, Os Module

---
+++ 01-node-tutorial/3-modules.js
@@ -0,0 +1,17 @@
+// CommonJS, every file is module (by default)
+// Modules - Encapsulated Code (only share minimum)
+
+// const names = require("./4-name");
+
+const sayHi = require("./5-utils");
+
+// sayHi("susan");
+// sayHi(names.john);
+// sayHi(names.peter);
+// sayHi(john)
+// sayHi(peter)
+
+// const alternative = require("./6-alternative-flavor")
+// console.log(alternative);
+
+require("./7-function-invoked");
