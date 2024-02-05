43bfb5bc03a38f7195b50a1bf3dceb58258f7ce2
Kaadirm <141996672+Kaadirm@users.noreply.github.com>
2024-02-05 09:56:50 +0300

    node tutorial started, general concept and globals

---
+++ 01-node-tutorial/2-globals.js
@@ -0,0 +1,14 @@
+// GLOBALS      - NO WINDOW !!!!
+// __dirname    - path to current directory
+// require      - function to use modules (CommonJS)
+// module       - info about current module (file)
+// process      - info about env where the program is being executed
+
+
+// console.log(__dirname)
+// console.log(require)
+// console.log(process);
+
+// setInterval( () => {
+//     console.log("hello world from nodeJS");
+// }, 1000)
