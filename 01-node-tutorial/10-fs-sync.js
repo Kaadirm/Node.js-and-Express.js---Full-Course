c723685178a71a79f9165b32f09ff1c498604db6
Kaadirm <141996672+Kaadirm@users.noreply.github.com>
2024-02-05 16:12:30 +0300

    Fs Module(Sync)

---
+++ 01-node-tutorial/10-fs-sync.js
@@ -0,0 +1,11 @@
+// const fs = require("fs");
+const { readFileSync, writeFileSync, write } = require("fs");
+
+const first = readFileSync("./content/first.txt", "utf8");
+const second = readFileSync("./content/second.txt", "utf8");
+
+// console.log(first, second);
+
+writeFileSync("./content/result-sync.txt", `Here is the result ----- ${first} ----- ${second}`, {flag: "a"})
+
+console.log(readFileSync("./content/result-sync.txt", "utf8"));
