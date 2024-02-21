6ebdae4f1399430ad0146bed71a1fda19ad8f0c8
Kaadirm <141996672+Kaadirm@users.noreply.github.com>
2024-02-21 22:38:55 +0300

    http headers, response status codes

--- 2-express-tutorial/app.js
+++ 2-express-tutorial/app.js
@@ -1,14 +1,30 @@
-console.log('Express Tutorial');
+console.log("Express Tutorial");
 
-
-const http = require('http');
+const http = require("http");
 
 const server = http.createServer((req, res) => {
-    console.log("user hit the server");
-    res.end(`Hi this is the port 5000`)
-})
+  // console.log("user hit the server");
+  // res.end(`Hi this is the port 5000`)
+
+  // Checking request url and sending html file
+  if (req.url === "/") {
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.end(
+      `<html><body style='background-color: gray; text-align: center; display: flex; justify-content: center; align-items: center;'><h1 style='color: blue; font-size: 5rem'>Hi, this is the HOME PAGE!</h1></body></html>`
+    );
+  } else if (req.url === "/about") {
+    res.writeHead(200, { "Content-Type": "text/plain" });
+    res.end("This is the about page.");
+  } else {
+    res.writeHead(404, { "Content-Type": "text/html" });
+    res.write(
+      `<html><body style='background-color: gray; text-align: center; display: flex; justify-content: center; align-items: center;'><h1 style='color: red; font-size: 5rem'>404 Not Found</h1></body></html>`
+    );
+    res.end();
+  }
+});
 
 const port = 5000;
 server.listen(port, () => {
-    console.log(`server http://localhost:${port} adresinde çalışıyor`);
+  console.log(`server http://localhost:${port} adresinde çalışıyor`);
 });
