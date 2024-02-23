edfbe5cd7cbb9b5b6b08f3e151735089cc7a0057
Kaadirm <141996672+Kaadirm@users.noreply.github.com>
2024-02-23 11:28:24 +0300

    Sending a simple JSON response containing array of objecs

--- 2-express-tutorial/app.js
+++ 2-express-tutorial/app.js
@@ -1,26 +1,13 @@
 console.clear();
 
 const express = require("express");
-const path = require("path");
-
 const app = express();
+const port = 5000;
 
-const route = 5000;
+app.get("/", (req, res) => {
+  res.json([{name: "Kadir"}, {name: "Berk" }])
+})
 
-// setup static and middleware
-app.use(express.static("./navbar-app"))
-
-
-// app.get("/", (req, res) => {
-//   res.sendFile(path.resolve(__dirname, "./navbar-app/index.html"))
-//   adding to static assets
-//   SSR
-// });
-
-app.all("*", (req, res) => {
-  res.status(404).send(`<h1>NOT FOUND</h1>`);
-});
-
-app.listen(route, () => {
-  console.log(`listening ${route}`);
-});
+app.listen(port, () => {
+  console.log(`listening on port ${port}`);
+})
