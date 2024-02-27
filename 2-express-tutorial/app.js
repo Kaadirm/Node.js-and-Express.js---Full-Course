693608cde4bfb327e9735f537db09e2ca08d6b49
Kaadirm <141996672+Kaadirm@users.noreply.github.com>
2024-02-27 11:04:25 +0300

    app.get method

--- 2-express-tutorial/app.js
+++ 2-express-tutorial/app.js
@@ -2,36 +2,12 @@ console.clear();
 
 const express = require("express");
 const app = express();
-const morgan = require("morgan")
-const logger = require("./logger");
-const authorize = require("./authorize");
 const port = 5000;
-// req => middleware => res
+let {people} = require("./data")
 
-// 1. use vs route
-// 2. options - our own / express / third party
-
-// app.use("/api", [authorize, logger]);
-// app.use(morgan("dev"))
-// app.use(morgan("combined"))
-app.use(morgan("tiny"))
-app.get("/", (req, res) => {
-  res.send("Home");
-});
-
-app.get("/about", (req, res) => {
-  res.send("About");
-});
-app.get("/api", (req, res) => {
-  // console.log(req.user)
-  res.send("API");
-});
-app.get("/api/products", (req, res) => {
-  res.send("API Products");
-});
-app.get("/api/items", (req, res) => {
-  res.send("API Items");
-});
+app.get("/api/people", (req, res) => {
+  res.status(200).json({success:true, data: people})
+})
 
 app.listen(port, () => {
   console.log(`listening on port ${port}`);
