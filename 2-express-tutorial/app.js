80c6003c4427ddaff92b3d36d99a6cd78771e46e
Kaadirm <141996672+Kaadirm@users.noreply.github.com>
2024-02-27 12:41:37 +0300

    post method basic demonstration

--- 2-express-tutorial/app.js
+++ 2-express-tutorial/app.js
@@ -3,11 +3,25 @@ console.clear();
 const express = require("express");
 const app = express();
 const port = 5000;
-let {people} = require("./data")
+let { people } = require("./data");
+
+// static assets
+app.use("/", express.static("./methods-public"));
+// parse form data
+app.use(express.urlencoded({ extended: false }));
 
 app.get("/api/people", (req, res) => {
-  res.status(200).json({success:true, data: people})
-})
+  res.status(200).json({ success: true, data: people });
+});
+
+app.post("/login", (req, res) => {
+  // console.log(req.body);
+  const {name} = req.body;
+  if(name){
+    return res.status(200).send(`Welcome ${name}`);
+  }
+  res.status(401).send("Please Provide Credentials");
+});
 
 app.listen(port, () => {
   console.log(`listening on port ${port}`);
