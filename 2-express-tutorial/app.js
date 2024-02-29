da362691766cd1a65b736155cbb57aaf15ddc554
Kaadirm <141996672+Kaadirm@users.noreply.github.com>
2024-02-29 09:03:52 +0300

    handling post request and sending back to client side

--- 2-express-tutorial/app.js
+++ 2-express-tutorial/app.js
@@ -9,15 +9,25 @@ let { people } = require("./data");
 app.use("/", express.static("./methods-public"));
 // parse form data
 app.use(express.urlencoded({ extended: false }));
-
+// parse json
+app.use(express.json())
 app.get("/api/people", (req, res) => {
   res.status(200).json({ success: true, data: people });
 });
 
+app.post("/api/people", (req, res) => {
+  const { name } = req.body;
+  if(!name){
+    return res.status(400).json({success: false, msg: "please provide name value"})
+  }
+  console.log(name);
+  res.status(201).json({success: true, person: name});
+});
+
 app.post("/login", (req, res) => {
   // console.log(req.body);
-  const {name} = req.body;
-  if(name){
+  const { name } = req.body;
+  if (name) {
     return res.status(200).send(`Welcome ${name}`);
   }
   res.status(401).send("Please Provide Credentials");
