30a5de9d4c27b8d523d76f45483dfca5cbd215c4
Kaadirm <141996672+Kaadirm@users.noreply.github.com>
2024-03-20 11:28:40 +0300

    server started and route structure created

--- 03-task-manager/starter/app.js
+++ 03-task-manager/starter/app.js
@@ -1,1 +1,18 @@
-console.log('Task Manager App')
+const express = require("express");
+const app = express();
+const PORT = 3000;
+
+// routes
+app.get("/hello", (req, res) => {
+  res.send("Task Manager App");
+});
+
+// app.get("/api/v1/tasks") get all the tasks
+// app.post("/api/v1/tasks") create a new task
+// app.get("/api/v1/tasks/:id") get a single task
+// app.patch("/api/v1/tasks/:id") update a task
+// app.delete("/api/v1/tasks/:id") delete a task
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
