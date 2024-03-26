ad70b61f3c21381878b87c5d4fae3dc469d7ab2d
Kaadirm <141996672+Kaadirm@users.noreply.github.com>
2024-03-26 15:42:45 +0300

    schema for db

---
+++ 03-task-manager/starter/models/Task.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+
+const mongoose = require("mongoose");
+
+const TaskSchema = new mongoose.Schema({
+  name: String,
+  completed: Boolean,
+});
+
+module.exports = mongoose.model("Task", TaskSchema);
+
+// const taskSchema = new mongoose.Schema({
+//     title: {
+//         type: String,
+//         required: true
+//     },
+//     description: {
+//         type: String,
+//         required: true
+//     },
+//     completed: {
+//         type: Boolean,
+//         default: false
+//     },
+//     createdAt: {
+//         type: Date,
+//         default: Date.now
+//     }
+// });
