4e43df2af0912428368be12cf358c626f4fa60e0
Kaadirm <141996672+Kaadirm@users.noreply.github.com>
2024-03-26 17:11:27 +0300

    validation for name objects

--- 03-task-manager/starter/models/Task.js
+++ 03-task-manager/starter/models/Task.js
@@ -1,27 +1,17 @@
 const mongoose = require("mongoose");
 
 const TaskSchema = new mongoose.Schema({
-  name: String,
-  completed: Boolean,
+  name: {
+    type: String,
+    required: [true, "Name is required."],
+    minlength: [3, "Name must be at least 3 characters long."],
+    maxlength: [20, "Name cannot exceed 20 characters."],
+    trim: true,
+  },
+  completed: {
+    type: Boolean,
+    default: false,
+  },
 });
 
 module.exports = mongoose.model("Task", TaskSchema);
-
-// const taskSchema = new mongoose.Schema({
-//     title: {
-//         type: String,
-//         required: true
-//     },
-//     description: {
-//         type: String,
-//         required: true
-//     },
-//     completed: {
-//         type: Boolean,
-//         default: false
-//     },
-//     createdAt: {
-//         type: Date,
-//         default: Date.now
-//     }
-// });
