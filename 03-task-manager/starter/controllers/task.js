35d05c24d9cc9f0d158a0264c611ff2ef2f541fe
Kaadirm <141996672+Kaadirm@users.noreply.github.com>
2024-03-27 14:55:20 +0300

    getting specific data with Mongoose findOne or findById method through request.params

--- 03-task-manager/starter/controllers/task.js
+++ 03-task-manager/starter/controllers/task.js
@@ -18,12 +18,25 @@ const createTask = async (req, res) => {
   }
 };
 
-const getTask = (req, res) => {
-  res.json({ id: req.params.id });
+const getTask = async (req, res) => {
+  try {
+    const { id: taskID } = req.params;
+    // const task = await Task.findById(req.params.id);
+    const task = await Task.findOne({ _id: taskID });
+    if (!task) {
+      return res.status(404).json({ msg: `No task with id : ${taskID}` });
+    } else {
+      return res.status(200).json({ task });
+    }
+  } catch (error) {
+    res.status(500).json({ msg: error.message });
+  }
 };
+
 const updateTask = (req, res) => {
   res.send("update task");
 };
+
 const deleteTask = (req, res) => {
   res.send("delete task");
 };
