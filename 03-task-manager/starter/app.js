e340c4fa62fb7666738ab0f79ed1bd48a239636c
Kaadirm <141996672+Kaadirm@users.noreply.github.com>
2024-03-28 14:54:52 +0300

    Port Variable Setting

--- 03-task-manager/starter/app.js
+++ 03-task-manager/starter/app.js
@@ -16,19 +16,14 @@ app.use("/api/v1/tasks", tasks);
 // not found route
 app.use(notFound);
 app.use(errorHandlerMiddleware);
-// app.get("/api/v1/tasks") get all the tasks
-// app.post("/api/v1/tasks") create a new task
-// app.get("/api/v1/tasks/:id") get a single task
-// app.patch("/api/v1/tasks/:id") update a task
-// app.delete("/api/v1/tasks/:id") delete a task
 
-const PORT = 3000;
+const port = process.env.PORT || 3000;
 
 const start = async () => {
   try {
     await connectDB(process.env.MONGO_URI);
-    app.listen(PORT, () => {
-      console.log(`Server is running on port ${PORT}`);
+    app.listen(port, () => {
+      console.log(`Server is running on port ${port}`);
     });
   } catch (error) {
     console.error(error);
