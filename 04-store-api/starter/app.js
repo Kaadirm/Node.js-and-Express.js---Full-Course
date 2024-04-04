286e3e94acbaef551c3a7ae35565059ceb19159c
Kaadirm <141996672+Kaadirm@users.noreply.github.com>
2024-04-04 23:44:15 +0300

    server started before db connection

--- 04-store-api/starter/app.js
+++ 04-store-api/starter/app.js
@@ -1,1 +1,41 @@
-console.log('04 Store API')
+console.clear();
+
+require("dotenv").config();
+// async errors
+
+const express = require("express");
+const app = express();
+
+const notFoundMiddleware = require("./middleware/not-found");
+const errorMiddleware = require("./middleware/error-handler");
+
+// middleware
+app.use(express.json());
+
+// routes
+app.get("/", (req, res) => {
+  res.send("<h1>Store API</h1><a href='/api/v1/products'>products route</a>");
+});
+
+// products route
+app.get("/api/v1/products", (req, res) => {
+  res.json({ message: "products" });
+});
+
+app.use(notFoundMiddleware);
+app.use(errorMiddleware);
+
+const PORT = process.env.PORT || 3000;
+
+const start = async () => {
+  try {
+    // connect to DB
+    app.listen(PORT, () => {
+      console.log(`Server is listening on port ${PORT}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
