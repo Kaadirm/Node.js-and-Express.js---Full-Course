292f9d9965f19e5a353e22ecdf94ae1ba206606a
Kaadirm <141996672+Kaadirm@users.noreply.github.com>
2024-04-05 13:20:42 +0300

    router build for /api/v1/products

--- 04-store-api/starter/app.js
+++ 04-store-api/starter/app.js
@@ -6,6 +6,7 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const connectDB = require("./db/connect");
+const productsRouter = require("./routes/products");
 
 const notFoundMiddleware = require("./middleware/not-found");
 const errorMiddleware = require("./middleware/error-handler");
@@ -19,18 +20,17 @@ app.get("/", (req, res) => {
 });
 
 // products route
-app.get("/api/v1/products", (req, res) => {
-  res.json({ message: "products" });
-});
+app.use("/api/v1/products", productsRouter);
 
+// 404 route
 app.use(notFoundMiddleware);
+// error handler
 app.use(errorMiddleware);
 
 const PORT = process.env.PORT || 3000;
 
 const start = async () => {
   try {
-    // connect to DB
     await connectDB(process.env.MONGO_URI);
     app.listen(PORT, () => {
       console.log(`Server is listening on port ${PORT}`);
