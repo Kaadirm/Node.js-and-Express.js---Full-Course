7cd264d1507ac92820694ae17f92b32d2c505c39
Kaadirm <141996672+Kaadirm@users.noreply.github.com>
2024-04-08 15:49:10 +0300

    Modified code for querying products based on request parameters.

--- 04-store-api/starter/controllers/products.js
+++ 04-store-api/starter/controllers/products.js
@@ -1,7 +1,7 @@
 const Product = require("../models/product");
 
 const getAllProductsStatic = async (req, res) => {
-  // const products = await Product.find({ rating: { $gt: 4.7 } }) 
+  // const products = await Product.find({ rating: { $gt: 4.7 } })
   // const products = await Product.find({
   //   featured: true,
   //   nbHits: products.length,
@@ -9,13 +9,35 @@ const getAllProductsStatic = async (req, res) => {
   // const products = await Product.find({ name: "vase table" });
 
   const products = await Product.find({ name: "vase table" });
-  res.status(200).json({ products, nbHits: products.length});
+  res.status(200).json({ products, nbHits: products.length });
 };
 
 const getAllProducts = async (req, res) => {
-  const filters = req.query;
-  const products = await Product.find(filters);
-  res.status(200).json({ products, nbHits: products.length });
+  const { name, price, featured, rating, company } = req.query;
+  const queryObject = {};
+
+  if (name) {
+    queryObject.name = { $regex: name, $options: "i" };
+  }
+
+  if (price) {
+    queryObject.price = { $gte: price };
+  }
+
+  if (featured) {
+    queryObject.featured = featured === "true" ? true : false;
+  }
+
+  if (rating) {
+    queryObject.rating = { $gte: rating };
+  }
+
+  if (company) {
+    queryObject.company = company;
+  }
+
+  const products = await Product.find(queryObject);
+  res.status(200).json({ nbHits: products.length, products });
 };
 
 module.exports = {
