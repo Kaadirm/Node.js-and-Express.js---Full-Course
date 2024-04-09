14050834666db457f7160c302f4a5afcc73ebd3c
Kaadirm <141996672+Kaadirm@users.noreply.github.com>
2024-04-09 11:02:56 +0300

    select specific fields

--- 04-store-api/starter/controllers/products.js
+++ 04-store-api/starter/controllers/products.js
@@ -8,12 +8,12 @@ const getAllProductsStatic = async (req, res) => {
   // });
   // const products = await Product.find({ name: "vase table" });
 
-  const products = await Product.find({}).sort("-name -price");
+  const products = await Product.find({}).select("name company");
   res.status(200).json({ products, nbHits: products.length });
 };
 
 const getAllProducts = async (req, res) => {
-  const { name, price, featured, rating, company, sort } = req.query;
+  const { name, price, featured, rating, company, sort, fields } = req.query;
   const queryObject = {};
 
   if (name) {
@@ -36,14 +36,23 @@ const getAllProducts = async (req, res) => {
     queryObject.company = company;
   }
 
-  let products;
+  let result = Product.find(queryObject);
+
+  // sort
   if (sort) {
-    const sortOptions = sort.split(",").join(" ");
-    products = await Product.find(queryObject).sort(sortOptions);
+    const sortList = sort.split(",").join(" ");
+    result = result.sort(sortList);
   } else {
-    products = await Product.find(queryObject);
+    result = result.sort("createdAt");
   }
 
+  // fields
+  if (fields) {
+    const fieldsList = fields.split(",").join(" ");
+    result = result.select(fieldsList);
+  }
+
+  const products = await result;
   res.status(200).json({ nbHits: products.length, products });
 };
 
