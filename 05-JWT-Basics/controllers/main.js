c859dffc1250949d5072c264c54709dd5df2a632
Kaadirm <141996672+Kaadirm@users.noreply.github.com>
2024-04-11 20:00:51 +0300

    signed in user, sent back JWT to the frontend (Just for sample)

--- 05-JWT-Basics/controllers/main.js
+++ 05-JWT-Basics/controllers/main.js
@@ -1,8 +1,8 @@
 // check username, password in post(login) request
 // if exist, create new JWT
 // send back to frontend
-
 // setup authentication so only the request with JWT can access the dashboard
+const jwt = require("jsonwebtoken");
 const CustomAPIError = require("../errors/custom-error");
 
 const login = async (req, res) => {
@@ -10,12 +10,20 @@ const login = async (req, res) => {
   // mongoose validation
   // Joi
   // check in the controller
-  if(!username || !password) {
-    throw new CustomAPIError( "Please provide email and password", 400)
+  if (!username || !password) {
+    throw new CustomAPIError("Please provide email and password", 400);
   }
 
-  console.log(`username: ${username}, password: ${password}`);
-  res.send("Fake Login/Register/Signup Route");
+  // just for demo, normally provided by DB!!
+  const id = new Date().getDate();
+
+  // try to keep payload small, better experience for user
+  // just for demo, in production use long, complex and unguessable string value!!!!!
+  const token = jwt.sign({ id, username }, process.env.JWT_SECRET, {
+    expiresIn: "30d",
+  });
+
+  res.status(200).json({ msg: "user created", token });
 };
 
 const dashboard = async (req, res) => {
