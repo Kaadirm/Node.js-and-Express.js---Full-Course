d53a2c729a086c76a756713e55129a9576aa643f
Kaadirm <141996672+Kaadirm@users.noreply.github.com>
2024-04-16 14:30:03 +0300

    Hashing Passwords with Mongoose pre save

--- 06-jobs-api/models/User.js
+++ 06-jobs-api/models/User.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -23,6 +24,9 @@ const userSchema = new mongoose.Schema({
   },
 });
 
-const User = mongoose.model("User", userSchema);
+userSchema.pre("save", async function () {
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
 
-module.exports = User;
+module.exports = mongoose.model("User", userSchema);
