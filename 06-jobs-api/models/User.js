d7babffdc49c224bf4ffc49f7c159582d141b9d1
Kaadirm <141996672+Kaadirm@users.noreply.github.com>
2024-04-17 09:43:41 +0300

    JWT with mongoose.methods

--- 06-jobs-api/models/User.js
+++ 06-jobs-api/models/User.js
@@ -1,7 +1,8 @@
 const mongoose = require("mongoose");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
-const userSchema = new mongoose.Schema({
+const UserSchema = new mongoose.Schema({
   name: {
     type: String,
     required: [true, "Please provide a name"],
@@ -24,9 +25,18 @@ const userSchema = new mongoose.Schema({
   },
 });
 
-userSchema.pre("save", async function () {
+UserSchema.pre("save", async function () {
   const salt = await bcrypt.genSalt(10);
   this.password = await bcrypt.hash(this.password, salt);
 });
 
-module.exports = mongoose.model("User", userSchema);
+UserSchema.methods.createJWT = function () {
+  // return jwt.sign({ userId: this._id, name: this.name }, process.env.JWT_SECRET, {
+  //   expiresIn: process.env.JWT_LIFETIME,
+  // });
+  return jwt.sign({ userId: this._id, name: this.name }, "jwtSecret", {
+    expiresIn: "30d",
+  });
+};
+
+module.exports = mongoose.model("User", UserSchema);
