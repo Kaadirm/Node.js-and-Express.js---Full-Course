2d7eb2a996e306514ebea48d85777838d2fb2e71
Kaadirm <141996672+Kaadirm@users.noreply.github.com>
2024-11-06 19:52:39 +0300

    Swagger UI setup

--- 06-jobs-api/app.js
+++ 06-jobs-api/app.js
@@ -1,47 +1,50 @@
-require("dotenv").config();
-require("express-async-errors");
+require('dotenv').config();
+require('express-async-errors');
 
 // extra security packages imports
-const helmet = require("helmet");
-const cors = require("cors");
-const xss = require("xss-clean");
-const rateLimit = require("express-rate-limit");
+const helmet = require('helmet');
+const cors = require('cors');
+const xss = require('xss-clean');
+const rateLimit = require('express-rate-limit');
 
-const express = require("express");
+// Swagger
+const swaggerUI = require('swagger-ui-express');
+
+const express = require('express');
 const app = express();
 
 // extra security packages usage
 // app.set("trust proxy", 1); if it is behinde a reverse proxy (Heroku, Bluemix, AWS ELB, Nginx, etc)
 app.use(
-  rateLimit({
-    windowMs: 15 * 60 * 1000, // 15 minutes
-    max: 100, // limit each IP to 100 requests per windowMs
-  })
+    rateLimit({
+        windowMs: 15 * 60 * 1000, // 15 minutes
+        max: 100 // limit each IP to 100 requests per windowMs
+    })
 );
 app.use(helmet());
 app.use(cors());
 app.use(xss());
 
 // connectDB
-const connectDB = require("./db/connect");
+const connectDB = require('./db/connect');
 
 // middleware
-const authenticateUser = require("./middleware/authentication");
+const authenticateUser = require('./middleware/authentication');
 
 // router
-const authRouter = require("./routes/auth");
-const jobsRouter = require("./routes/jobs");
+const authRouter = require('./routes/auth');
+const jobsRouter = require('./routes/jobs');
 
 // error handler
-const notFoundMiddleware = require("./middleware/not-found");
-const errorHandlerMiddleware = require("./middleware/error-handler");
+const notFoundMiddleware = require('./middleware/not-found');
+const errorHandlerMiddleware = require('./middleware/error-handler');
 
 app.use(express.json());
 // extra packages
 
 // routes
-app.use("/api/v1/auth", authRouter);
-app.use("/api/v1/jobs", authenticateUser, jobsRouter);
+app.use('/api/v1/auth', authRouter);
+app.use('/api/v1/jobs', authenticateUser, jobsRouter);
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
@@ -49,14 +52,12 @@ app.use(errorHandlerMiddleware);
 const port = process.env.PORT || 3000;
 
 const start = async () => {
-  try {
-    await connectDB(process.env.MONGO_URI);
-    app.listen(port, () =>
-      console.log(`Server is listening on port ${port}...`)
-    );
-  } catch (error) {
-    console.log(error);
-  }
+    try {
+        await connectDB(process.env.MONGO_URI);
+        app.listen(port, () => console.log(`Server is listening on port ${port}...`));
+    } catch (error) {
+        console.log(error);
+    }
 };
 
 start();
