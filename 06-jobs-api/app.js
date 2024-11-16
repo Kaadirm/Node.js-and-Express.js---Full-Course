526248996a6a5e50e2c2d8f4a18009dfe9c4c7b1
Kaadirm <141996672+Kaadirm@users.noreply.github.com>
2024-11-16 19:56:13 +0300

    Load Swagger documentation from YAML file in app.js

--- 06-jobs-api/app.js
+++ 06-jobs-api/app.js
@@ -10,6 +10,7 @@ const rateLimit = require('express-rate-limit');
 // Swagger
 const swaggerUI = require('swagger-ui-express');
 const YAML = require('yamljs');
+const swaggerDocument = YAML.load('./swagger.yaml');
 
 const express = require('express');
 const app = express();
