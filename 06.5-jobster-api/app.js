dd5b8d439d339fa738761b74fa32d401af56ca46
Kaadirm <141996672+Kaadirm@users.noreply.github.com>
2024-11-21 16:52:52 +0300

    removed swagger load

--- 06.5-jobster-api/app.js
+++ 06.5-jobster-api/app.js
@@ -7,11 +7,6 @@ const cors = require('cors');
 const xss = require('xss-clean');
 const rateLimiter = require('express-rate-limit');
 
-// Swagger
-const swaggerUI = require('swagger-ui-express');
-const YAML = require('yamljs');
-const swaggerDocument = YAML.load('./swagger.yaml');
-
 const express = require('express');
 const app = express();
 
@@ -26,10 +21,10 @@ const errorHandlerMiddleware = require('./middleware/error-handler');
 
 app.set('trust proxy', 1);
 app.use(
-  rateLimiter({
-    windowMs: 15 * 60 * 1000, // 15 minutes
-    max: 100, // limit each IP to 100 requests per windowMs
-  })
+    rateLimiter({
+        windowMs: 15 * 60 * 1000, // 15 minutes
+        max: 100 // limit each IP to 100 requests per windowMs
+    })
 );
 app.use(express.json());
 app.use(helmet());
@@ -37,7 +32,7 @@ app.use(cors());
 app.use(xss());
 
 app.get('/', (req, res) => {
-  res.send('<h1>Jobs API</h1><a href="/api-docs">Documentation</a>');
+    res.send('<h1>Jobs API</h1><a href="/api-docs">Documentation</a>');
 });
 app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 
@@ -51,14 +46,12 @@ app.use(errorHandlerMiddleware);
 const port = process.env.PORT || 5000;
 
 const start = async () => {
-  try {
-    await connectDB(process.env.MONGO_URI);
-    app.listen(port, () =>
-      console.log(`Server is listening on port ${port}...`)
-    );
-  } catch (error) {
-    console.log(error);
-  }
+    try {
+        await connectDB(process.env.MONGO_URI);
+        app.listen(port, () => console.log(`Server is listening on port ${port}...`));
+    } catch (error) {
+        console.log(error);
+    }
 };
 
 start();
