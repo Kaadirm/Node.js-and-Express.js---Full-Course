1b56042edad5d32fa8eea9eb074539ab27a84b9a
Kaadirm <141996672+Kaadirm@users.noreply.github.com>
2024-11-22 16:54:09 +0300

    removed swagger api route

--- 06.5-jobster-api/app.js
+++ 06.5-jobster-api/app.js
@@ -31,11 +31,6 @@ app.use(helmet());
 app.use(cors());
 app.use(xss());
 
-app.get('/', (req, res) => {
-    res.send('<h1>Jobs API</h1><a href="/api-docs">Documentation</a>');
-});
-app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
-
 // routes
 app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/jobs', authenticateUser, jobsRouter);
