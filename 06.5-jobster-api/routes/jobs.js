a25eb263c27dc8a4776327b42d19e5838d4c8c44
Kaadirm <141996672+Kaadirm@users.noreply.github.com>
2024-12-25 22:10:53 +0300

    Add route for job statistics retrieval

--- 06.5-jobster-api/routes/jobs.js
+++ 06.5-jobster-api/routes/jobs.js
@@ -2,9 +2,10 @@ const express = require('express');
 const testUser = require('../middleware/testUser');
 
 const router = express.Router();
-const { createJob, deleteJob, getAllJobs, updateJob, getJob } = require('../controllers/jobs');
+const { createJob, deleteJob, getAllJobs, updateJob, getJob, showStats } = require('../controllers/jobs');
 
 router.route('/').post(testUser, createJob).get(getAllJobs);
+router.route('/stats').get(showStats);
 
 router.route('/:id').get(getJob).delete(testUser, deleteJob).patch(testUser, updateJob);
 
