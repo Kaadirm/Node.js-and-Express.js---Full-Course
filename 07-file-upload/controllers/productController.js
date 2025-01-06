be645c7174c1b5edff12f731e7e123f742bc09ad
Kaadirm <141996672+Kaadirm@users.noreply.github.com>
2025-01-07 00:06:49 +0300

    Add createProduct function to productController for product creation

---
+++ 07-file-upload/controllers/productController.js
@@ -0,0 +1,6 @@
+const Product = require('../models/Product');
+const { StatusCodes } = require('http-status-codes');
+
+const createProduct = async (req, res) => {
+    res.send('create product');
+};
