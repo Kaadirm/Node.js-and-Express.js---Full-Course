5131196e9102741dd51667c1acf7d9c916fef9dd
Kaadirm <141996672+Kaadirm@users.noreply.github.com>
2025-01-08 00:07:58 +0300

    Add getAllProducts function to productController for retrieving product list

--- 07-file-upload/controllers/productController.js
+++ 07-file-upload/controllers/productController.js
@@ -4,3 +4,12 @@ const { StatusCodes } = require('http-status-codes');
 const createProduct = async (req, res) => {
     res.send('create product');
 };
+
+const getAllProducts = async (req, res) => {
+    res.send('list of products');
+};
+
+module.exports = {
+    createProduct,
+    getAllProducts
+};
