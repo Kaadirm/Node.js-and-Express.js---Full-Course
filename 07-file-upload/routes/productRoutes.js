5ea4d4aa806b83e095dc7d624df403956851aef8
Kaadirm <141996672+Kaadirm@users.noreply.github.com>
2025-01-11 00:17:43 +0300

    Add routes for product creation, retrieval, and image uploads

--- 07-file-upload/routes/productRoutes.js
+++ 07-file-upload/routes/productRoutes.js
@@ -3,3 +3,8 @@ const router = express.Router();
 
 const { createProduct, getAllProducts } = require('../controllers/productController');
 const { uploadProductImage } = require('../controllers/uploadsController');
+
+router.route('/').post(createProduct).get(getAllProducts);
+router.route('/uploads').post(uploadProductImage);
+
+module.exports = router;
