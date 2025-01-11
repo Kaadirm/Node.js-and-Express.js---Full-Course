69ba05e5e588026aeb1b7f69d02a6ec0cd42e70e
Kaadirm <141996672+Kaadirm@users.noreply.github.com>
2025-01-12 00:18:06 +0300

    Add product router to main application file

--- 07-file-upload/app.js
+++ 07-file-upload/app.js
@@ -7,13 +7,18 @@ const app = express();
 // database
 const connectDB = require('./db/connect');
 
+// product router
+const productRouter = require('./routes/productRoutes');
+
 // error handler
 const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
 
 app.get('/', (req, res) => {
-  res.send('<h1>File Upload Starter</h1>');
+    res.send('<h1>File Upload Starter</h1>');
 });
+
+app.use('/api/v1/products', productRouter);
 
 // middleware
 app.use(notFoundMiddleware);
@@ -22,15 +27,13 @@ app.use(errorHandlerMiddleware);
 const port = process.env.PORT || 3000;
 
 const start = async () => {
-  try {
-    await connectDB(process.env.MONGO_URI);
+    try {
+        await connectDB(process.env.MONGO_URI);
 
-    app.listen(port, () =>
-      console.log(`Server is listening on port ${port}...`)
-    );
-  } catch (error) {
-    console.log(error);
-  }
+        app.listen(port, () => console.log(`Server is listening on port ${port}...`));
+    } catch (error) {
+        console.log(error);
+    }
 };
 
 start();
