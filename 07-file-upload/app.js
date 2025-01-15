d554a2fe220bcac212bad736e745c7001b5fbadc
Kaadirm <141996672+Kaadirm@users.noreply.github.com>
2025-01-16 00:18:37 +0300

    Add JSON parsing middleware to the application

--- 07-file-upload/app.js
+++ 07-file-upload/app.js
@@ -14,6 +14,8 @@ const productRouter = require('./routes/productRoutes');
 const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
 
+app.use(express.json());
+
 app.get('/', (req, res) => {
     res.send('<h1>File Upload Starter</h1>');
 });
