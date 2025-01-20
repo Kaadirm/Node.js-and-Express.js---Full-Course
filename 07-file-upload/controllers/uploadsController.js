997ad0edd49e51f9968cf2d2db97a7166886d290
Kaadirm <141996672+Kaadirm@users.noreply.github.com>
2025-01-20 23:20:09 +0300

    Implement image upload functionality in uploadsController

--- 07-file-upload/controllers/uploadsController.js
+++ 07-file-upload/controllers/uploadsController.js
@@ -1,7 +1,13 @@
+const path = require('path');
+
 const { StatusCodes } = require('http-status-codes');
 
 const uploadProductImage = async (req, res) => {
-    console.log(req.files)
+    const productImage = req.files.image;
+
+    const imagePath = path.join(__dirname, `../uploads/${productImage.name}`);
+
+    await productImage.mv(imagePath);
     res.send('upload product iamge');
 };
 
