9af9af5c66460e1a38b7980744cd098607fd72ed
Kaadirm <141996672+Kaadirm@users.noreply.github.com>
2025-02-01 00:11:09 +0300

    Configure Cloudinary for file uploads

--- 07-file-upload/app.js
+++ 07-file-upload/app.js
@@ -6,6 +6,13 @@ const app = express();
 
 // file upload
 const fileUpload = require('express-fileupload');
+// USE V2
+const cloudinary = require('cloudinary').v2;
+ cloudinary.config({ 
+        cloud_name: process.env.CLOUD_NAME,
+        api_key: process.env.CLOUD_API_KEY,
+        api_secret: process.env.CLOUD_API_SECRET
+    });
 
 // database
 const connectDB = require('./db/connect');
