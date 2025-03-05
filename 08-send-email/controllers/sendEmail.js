5387f2b50cfc4a1b98c7ce877d66758a83ffde32
Kadir <kadir.memisoglu@moneytolia.com>
2025-03-05 11:40:47 +0300

    Add Gmail email sending functionality to sendEmail controller, exporting sendEmailGmail method alongside SendGrid

--- 08-send-email/controllers/sendEmail.js
+++ 08-send-email/controllers/sendEmail.js
@@ -37,7 +37,33 @@ const sendEmailSendGrid = async (req, res) => {
     res.json(info)
 }
 
+const sendEmailGmail = async (req, res) => {
+    const transporter = nodemailer.createTransport({
+        host: 'smtp.gmail.com',
+        port: 587,
+        secure: false, // true for 465, false for other ports
+        auth: {
+            user: process.env.EMAIL_USER, // your gmail
+            pass: process.env.EMAIL_PASS  // your app password
+        }
+    });
+    
+    let info = await transporter.sendMail({
+        from: process.env.EMAIL_USER, // sender address (your Gmail)
+        to: 'aptul_kadir1995@hotmail.com',   // list of receivers
+        subject: 'Hello from Gmail',   // Subject line
+        text: 'Hello world?',          // plain text body
+        html: '<h2>Sending Emails with Gmail via Node.js</h2>' // html body
+    });
+
+    res.json({
+        message: 'Email sent successfully!',
+        messageId: info.messageId,
+    });
+};
+
 module.exports = {
     sendEmailEthereal,
-    sendEmailSendGrid
+    sendEmailSendGrid,
+    sendEmailGmail
 }
