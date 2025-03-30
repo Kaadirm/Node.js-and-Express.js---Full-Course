d8964c198177422faedc6ac44a86dcb524e3f97a
Kadir <kadir.memisoglu@moneytolia.com>
2025-03-30 18:18:31 +0300

    Add sendEmail and sendVerificationEmail utilities to index.js for email handling and verification

--- 11-auth-workflow/server/utils/index.js
+++ 11-auth-workflow/server/utils/index.js
@@ -1,10 +1,15 @@
 const { createJWT, isTokenValid, attachCookiesToResponse } = require('./jwt');
 const createTokenUser = require('./createTokenUser');
 const checkPermissions = require('./checkPermissions');
+const sendEmail = require('./sendEmail');
+const sendVerificationEmail = require('./sendVerificationEmail');
+
 module.exports = {
   createJWT,
   isTokenValid,
   attachCookiesToResponse,
   createTokenUser,
   checkPermissions,
+  sendEmail,
+  sendVerificationEmail
 };
