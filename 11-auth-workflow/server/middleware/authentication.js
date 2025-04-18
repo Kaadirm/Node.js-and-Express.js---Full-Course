8a1838bf400d14e1dca53229ae6b8a7edd2391df
Kadir <kadir.memisoglu@moneytolia.com>
2025-04-18 22:45:57 +0300

    Enhance authentication middleware to support refresh token validation and attach cookies to response, improving user authentication flow and security.

--- 11-auth-workflow/server/middleware/authentication.js
+++ 11-auth-workflow/server/middleware/authentication.js
@@ -1,8 +1,9 @@
 const CustomError = require('../errors');
-const { isTokenValid } = require('../utils');
+const { isTokenValid, attachCookiesToResponse } = require('../utils');
+const Token = require('../models/Token');
 
 const authenticateUser = async (req, res, next) => {
-  const {accessToken} = req.signedCookies;
+  const {accessToken, refreshToken} = req.signedCookies;
 
   try {
     if(accessToken) {
@@ -10,7 +11,19 @@ const authenticateUser = async (req, res, next) => {
       req.user = payload.user;
       return next();
     }
-    
+    const payload = isTokenValid(refreshToken)
+
+    const existingToken = await Token.findOne({user: payload.user.userId, refreshToken: payload.refreshToken});
+
+    if(!existingToken || !existingToken?.isValid) {
+      throw new CustomError.UnauthenticatedError('Authentication Invalid');
+    }
+
+    attachCookiesToResponse({res, user: payload.user, refreshToken: existingToken.refreshToken});
+
+    req.user = payload.user;
+    next();
+
   } catch (error) {
     throw new CustomError.UnauthenticatedError('Authentication Invalid');
   }
