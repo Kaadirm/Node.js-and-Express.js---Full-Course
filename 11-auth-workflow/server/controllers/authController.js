8b81fb8589ad88fa79877fadb154bd693e2ba0b3
Kadir <kadir.memisoglu@moneytolia.com>
2025-04-20 10:02:33 +0300

    Update logout function to set accessToken and refreshToken cookies with correct settings, enhancing security and user experience during logout.

--- 11-auth-workflow/server/controllers/authController.js
+++ 11-auth-workflow/server/controllers/authController.js
@@ -117,10 +117,14 @@ const login = async (req, res) => {
 
 const logout = async (req, res) => {
   await Token.findOneAndDelete({user: req.user.userId});
-
-  res.cookie('token', 'logout', {
+  
+  res.cookie('accessToken', 'logout', {
     httpOnly: true,
-    expires: new Date(Date.now() + 1000),
+    expires: new Date(Date.now()),
+  });
+  res.cookie('refreshToken', 'logout', {
+    httpOnly: true,
+    expires: new Date(Date.now()),
   });
   res.status(StatusCodes.OK).json({ msg: 'user logged out!' });
 };
