8b8e89985e0621193e2986c4108eb20fc426f477
Kadir <kadir.memisoglu@moneytolia.com>
2025-04-21 10:02:38 +0300

    Update cookie expiration settings in attachCookiesToResponse function to enhance security and improve token management

--- 11-auth-workflow/server/utils/jwt.js
+++ 11-auth-workflow/server/utils/jwt.js
@@ -13,19 +13,19 @@ const attachCookiesToResponse = ({ res, user, refreshToken }) => {
   const accessTokenJWT = createJWT({ payload: {user} });
   const refreshTokenJWT = createJWT({ payload: {user, refreshToken} });
 
-  const oneDay = 1000 * 60 * 60 * 24;
+  const shortExp = 1000 * 60 * 15;
+  const longExp = 1000 * 60 * 60 * 24 * 7;
 
   res.cookie('accessToken', accessTokenJWT, {
     httpOnly: true,
-    // expires: new Date(Date.now() + oneDay),
+    expires: new Date(Date.now() + shortExp),
     secure: process.env.NODE_ENV === 'production',
     signed: true,
-    maxAge: 1000
   });
 
   res.cookie('refreshToken', refreshTokenJWT, {
     httpOnly: true,
-    expires: new Date(Date.now() + oneDay),
+    expires: new Date(Date.now() + longExp),
     secure: process.env.NODE_ENV === 'production',
     signed: true,
   });
