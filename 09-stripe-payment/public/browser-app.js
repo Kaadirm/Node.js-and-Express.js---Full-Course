c078fdfccbd2ffc4b89856279168b78207999165
Kadir <kadir.memisoglu@moneytolia.com>
2025-05-14 16:40:14 +0300

    Update Stripe public key in browser-app.js for payment processing

--- 09-stripe-payment/public/browser-app.js
+++ 09-stripe-payment/public/browser-app.js
@@ -6,7 +6,7 @@ const total_amount = 10998;
 const shipping_fee = 1099;
 
 var stripe = Stripe(
-  'pk_test_51I87djFp5pnuKUXgBVIHiR36vVAWyfuyb7ckrhgyDNA1kM0GWHas9ZGUAgwJSFNUxrbyE6NwlMNmls1iGSfzHDdE00DQB3y6AH'
+  'pk_test_51RwNYsCaqRZGH763dcLLImXMPOtkoFUIETwlFLMjIQ1WaS5yCHU9FAxc5FC4xrWnIpLl5sBxCzssALYPs3PEW873003oZOrkG7'
 );
 
 // The items the customer wants to buy
