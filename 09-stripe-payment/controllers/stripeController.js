7b6fae6846a485d47daf40810c0d40d4097986e1
Kadir <kadir.memisoglu@moneytolia.com>
2025-05-20 16:53:04 +0300

    import stripe in the controller and use secret key from env file

--- 09-stripe-payment/controllers/stripeController.js
+++ 09-stripe-payment/controllers/stripeController.js
@@ -1,6 +1,8 @@
-const stripe = async (req, res) => {
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+
+const stripeController = async (req, res) => {
   console.log(req.body);
   res.send('stripe route');
 };
 
-module.exports = stripe;
+module.exports = stripeController;
