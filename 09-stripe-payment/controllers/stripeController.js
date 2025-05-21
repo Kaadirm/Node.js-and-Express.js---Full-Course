97bcbf2944accf9ce6c14db0f33dd0d25af96739
Kadir <kadir.memisoglu@moneytolia.com>
2025-05-21 16:58:12 +0300

    Add order amount calculation in stripeController for payment processing

--- 09-stripe-payment/controllers/stripeController.js
+++ 09-stripe-payment/controllers/stripeController.js
@@ -1,7 +1,15 @@
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
 const stripeController = async (req, res) => {
-  console.log(req.body);
+  const { purchase, total_amount, shipping_fee } = req.body;
+
+  // Normally you would get this from your DB or an external API
+  // You get the id from the client side and then you can get the product details from your DB
+  // For demo purposes, we're using a what comes in from the client
+  const calculateOrderAmount = () => {
+    return total_amount + shipping_fee;
+  };
+
   res.send('stripe route');
 };
 
